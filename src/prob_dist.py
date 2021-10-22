80a2b80980dc5a42a66f0cf0cd55350de20fc002
Ahsan Ali <aali@nevada.unr.edu>
2021-10-22 01:01:41 -0700

    added arg parser

--- src/prob_dist.py
+++ src/prob_dist.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
 
 import h5py
+from simulation import Params
 import sys
 import multiprocessing as mp
-
+import argparse
 import os
 import utils
 
-
+parser = argparse.ArgumentParser(description='Bragg Peak representation encoding')
+parser.add_argument('-n_threads',   type=int, default=2, help='Number of Threads')
+parser.add_argument('-angle',type=int, default=128, help='Number of angles')
+parser.add_argument('-num_images', type=int,default=10, help='Number of images per thread')
+parser.add_argument('-ofn', type=str,required=True, help='ouput file name')
 
 def main():
-    if len(sys.argv)<5:
-        print('syntax: %s <outfilename.h5> <nimages> <nchannels> <nthreads>'%sys.argv[0])
-        return
-
-    paramslist = [Params('%s'%(sys.argv[1]),int(sys.argv[2])) for i in range(int(sys.argv[4]))]
+    args, unparsed = parser.parse_known_args()
+    if len(unparsed) > 0:
+        print('Unrecognized argument(s): \n%s \nProgram exiting ... ... ' % '\n'.join(unparsed))
+        exit(0)
+    if not os.path.exists(args.ofn):
+        os.makedirs(args.ofn)
+    paramslist = [Params('%s'%(args.ofn),args.num_images) for i in range(args.n_threads)]
     for p in paramslist:
-        p.setnangles(int(sys.argv[3])).setdrawscale(2).settestsplit(.1)
+        p.setnangles(args.angle).setdrawscale(2).settestsplit(.1)
 
     with mp.Pool(processes=len(paramslist)) as pool:
         pool.map(runprocess,paramslist)
