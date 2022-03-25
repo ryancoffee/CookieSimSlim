554f97367ae354cef3664a997bf24f7ab99db9df
Ryan Coffee <coffee@slac.stanford.edu>
2022-03-25 12:12:56 -0700

    args.mike True/False

--- src/collect_images.py
+++ src/collect_images.py
@@ -7,6 +7,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Collects the listed h5 files into a train block of images and a test block of images.')
 parser.add_argument('-ifnames', type=str,nargs='+',required=True, help='List of .h5 filenames from which to collect images.')
+parser.add_argument('-mike', type=bool,required=False,default=False,help='If this is set True, then the format will be for Mike K.')
 
 def main():
     args,unparsed = parser.parse_known_args()
@@ -19,7 +20,7 @@ def main():
         Ytrain = []
         Xtest = []
         Ytest = []
-        m = re.search('(^.*)\/(\w+.\d+)\.h5',fname)
+        m = re.search('(^.*)\/(\w+.pid\d+)\.h5',fname)
         if not m:
             print('failed filename match for ofname = %s'%fname)
             return
@@ -36,13 +37,25 @@ def main():
         ofnameTrain = '%s/%s.Train.h5'%(m.group(1),m.group(2))
         ofnameTest = '%s/%s.Test.h5'%(m.group(1),m.group(2))
         if len(Xtrain)>1:
+            if args.mike:
+                ax = 0
+            else:
+                ax = -1
+            x = np.stack(Xtrain,axis=ax)
+            y = np.stack(Ytrain,axis=ax)
             with h5py.File(ofnameTrain,'w') as f:
-                f.create_dataset('Xtrain',data=np.stack(Xtrain,axis=-1),dtype=np.uint8)
-                f.create_dataset('Ytrain',data=np.stack(Ytrain,axis=-1),dtype=np.float16)
+                f.create_dataset('Xtrain',data=x,dtype=np.uint8)
+                f.create_dataset('Ytrain',data=y,dtype=np.float16)
         if len(Xtest)>1:
+            if args.mike:
+                ax = 0
+            else:
+                ax = -1
+            x = np.stack(Xtest,axis=ax)
+            y = np.stack(Ytest,axis=ax)
             with h5py.File(ofnameTest,'w') as f:
-                f.create_dataset('Xtest',data=np.stack(Xtest,axis=-1),dtype=np.uint8)
-                f.create_dataset('Ytest',data=np.stack(Ytest,axis=-1),dtype=np.float16)
+                f.create_dataset('Xtest',data=x,dtype=np.uint8)
+                f.create_dataset('Ytest',data=y,dtype=np.float16)
     return
 
 if __name__ == '__main__':
