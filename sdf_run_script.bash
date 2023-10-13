cee322719d73e8006ad49f86fbd7cca7df052658
jhirschm <jhirschm@stanford.edu>
2023-10-13 13:02:29 -0700

    pulse qualification valid run script

---
+++ sdf_run_script.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=lcls
+#SBATCH --job-name=lstm
+#SBATCH --output=output-%j.txt
+#SBATCH --error=output-%j.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=10g
+#SBATCH --time=0-20:00:00
+#SBATCH --gpus geforce_rtx_2080_ti:0
+source ~/.bashrc
+conda activate pytorch-gpu
+export SLURM_EXACT=1
+echo starting run 1 at: `date`
+opath=/sdf/group/lcls/ds/scratch/jhirschm/MRCO_Sim_Data/CookieSimSlim_data
+mkdir -p ${opath}
+outfile=${opath}/data_small_Oct13_2023.h5
+python3 ./src/run_simulation.py -ofname ${outfile} -n_threads 16 -n_images 100 -n_angles 16 -n_energies 512 -polstrength 1 -polstrengthvar 1 -centralenergy 256 -centralenergyvar 256 -kickstrength 128 -kickstrengthvar 32 -drawscale 4
+
+# Print the date again -- when finished
+echo Finished at: `date`
