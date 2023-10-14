1530ac281f372d9cc85d29b78e226bc67ee9e0b8
jhirschm <jhirschm@stanford.edu>
2023-10-13 17:58:06 -0700

    pulse qualification codnition update

--- src/simulation.py
+++ src/simulation.py
@@ -203,7 +203,7 @@ def runprocess(params):
             key = keyhash.hexdigest()
             grp = f.create_group(key)
             X, Y = build_XY(params)
-            print("hereer")
+            print("firs here")
             print(params.sasephases)
             grp.create_dataset('Ypdf', data=Y, dtype=np.float32)
             hitsvec = []
@@ -230,7 +230,7 @@ def runprocess(params):
             grp.attrs.create('sasewidth', params.sasewidth,dtype=np.float16)
             grp.attrs.create('sasescale', params.sasescale,dtype=np.uint8)
             grp.attrs.create('sasecenters', params.sasecenters,dtype=np.float16)
-            print("hereer")
+            print("next here")
             print(params.sasephases)
             grp.attrs.create('sasephases', params.sasephases,dtype=np.float16)
             grp.attrs.create('saseamps', params.saseamps,dtype=np.float16)
@@ -274,31 +274,7 @@ def get_valid_phase_list(params,ncenters,energy_sase_width=None,phase_difference
     rng = params.rng
     valid = False
     phase_list = list(rng.random(ncenters)*2.*np.pi)
-    # # this seems good but then realized that changing one phase could then make another phase invalid so either need to loop again or just keep randomly selecting new phase list until valid
-    # actually randomly selecting new phase list until valid is not good because then this means if any energies are close, then enforces all phase differences  (not representative data anymore)
-    # for i in range(params.centralenergy):
-    #     energy = params.centralenergy[i]
-    #     energy_width = params.centralenergywidth[i]
-    #     phase = phase_list[i]
-    #     for j in range(params.centralenergy):
-    #         if i!=j:
-    #             energy2 = params.centralenergy[j]
-    #             energy_width2 = params.centralenergywidth[j]
-    #             phase2 = phase_list[j]
-    #             if np.abs(energy-energy2)<(energy_width+energy_width2):
-    #                 if np.abs(phase-phase2)<phase_difference_threshold:
-    #                     valid = False
-    #                     while not valid:
-    #                         new_phase = (rng.random(1)*2.*np.pi)
-    #                         if np.abs(new_phase-phase2)>phase_difference_threshold:
-    #                             phase_list[i] = new_phase
-    #                             valid = True
-    #                         else:
-    #                             valid = False
-    #                 else:
-    #                     phase_list = phase_list
-    #             else:
-    #                 phase_list = phase_list
+
 
     # actually need to wrap all in another while loop that runs through again until actually valid
     fully_valid = False
@@ -347,7 +323,6 @@ def build_XY(params):
     ncenters = rng.poisson(params.sasescale)
     params.setcenters( list(rng.normal(params.centralenergy,params.centralenergywidth,ncenters)) )
     valid_phase_list = get_valid_phase_list(params,ncenters, energy_sase_width = 0.5)
-    print(valid_phase_list)
     params.setphases(valid_phase_list)
     #params.setphases( list(rng.random(ncenters)*2.*np.pi) )
     params.setamps( [rng.poisson(10)/10 for i in range(ncenters)] )
