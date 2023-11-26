20a0dc3e00a95cd62d429b28bb9e0fd159cd8607
jhirschm <jhirschm@stanford.edu>
2023-11-25 18:40:45 -0800

    run script

--- sdf_run_script.bash
+++ sdf_run_script.bash
@@ -14,8 +14,8 @@ export SLURM_EXACT=1
 echo starting run 1 at: `date`
 opath=/sdf/group/lcls/ds/scratch/jhirschm/MRCO_Sim_Data/CookieSimSlim_data
 mkdir -p ${opath}
-outfile=${opath}/data_small_Oct13_2023.h5
-python3 ./src/run_simulation.py -ofname ${outfile} -n_threads 16 -n_images 10000 -n_angles 16 -n_energies 512 -polstrength 1 -polstrengthvar 1 -centralenergy 256 -centralenergyvar 256 -kickstrength 128 -kickstrengthvar 32 -drawscale 4
+outfile=${opath}/large_poisson_Nov25_2023.h5
+python3 ./src/run_simulation.py -ofname ${outfile} -n_threads 128 -n_images 8192 -n_angles 16 -n_energies 512 -polstrength 1 -polstrengthvar 1 -centralenergy 256 -centralenergyvar 256 -kickstrength 128 -kickstrengthvar 32 -drawscale 4
 
 # Print the date again -- when finished
 echo Finished at: `date`
