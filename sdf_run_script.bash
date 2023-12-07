e64092776dbf57e8b8ebf535a7ee88e8e5667eb8
jhirschm <jhirschm@stanford.edu>
2023-12-06 23:27:25 -0800

    5 class run

--- sdf_run_script.bash
+++ sdf_run_script.bash
@@ -15,7 +15,7 @@ echo starting run 1 at: `date`
 opath=/sdf/group/lcls/ds/scratch/jhirschm/MRCO_Sim_Data/CookieSimSlim_data/large_even_Nov29_2023_directory/
 mkdir -p ${opath}
 outfile=${opath}/large_even_fiveClass_Dec06_2023.h5
-python3 ./src/run_simulation.py -ofname ${outfile} -n_threads 64 -n_images 8192 -n_angles 16 -n_energies 512 -polstrength 1 -polstrengthvar 1 -centralenergy 256 -centralenergyvar 256 -kickstrength 128 -kickstrengthvar 32 -drawscale 4 -custom_evenly_distributed_sase True
+python3 ./src/run_simulation.py -ofname ${outfile} -n_threads 64 -n_images 8192 -n_angles 16 -n_energies 512 -polstrength 1 -polstrengthvar 1 -centralenergy 256 -centralenergyvar 256 -kickstrength 128 -kickstrengthvar 32 -drawscale 4 -custom_evenly_distributed_sase True -nclasses 5 -max_num_sase 10
 
 # Print the date again -- when finished
 echo Finished at: `date`
