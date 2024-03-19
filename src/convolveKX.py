da94a288aebc08d25a254a73ee1edee1ac780559
Ryan Coffee <coffeer76@gmail.com>
2024-03-19 11:20:49 -0700

    principle works

--- src/convolveKX.py
+++ src/convolveKX.py
@@ -63,33 +63,84 @@ def fillKernel(width,strength,kern):
 def main(fname):
     rng = np.random.default_rng()
     with h5py.File(fname,'r') as f:
-        shotkeys = [k for k in f.keys()]
+        shotkeys = [k for k in f.keys() if len(f[k].attrs['sasecenters'])>0]
         rng.shuffle(shotkeys)
-        ki = 0
-        k = shotkeys[ki]
-        while len(f[k].attrs['sasecenters']) !=1:
-            ki += 1
-            k = shotkeys[ki]
-        x = f[k]['Ximg'][()]
-        y = f[k]['Ypdf'][()]
-        wlist = f[k].attrs['sasewidth']*np.arange(.2,1.1,.1,dtype=float)
-        print(wlist)
-        slist = f[k].attrs['kickstrength']*np.arange(.25,4,.125,dtype=float)
-        print(slist)
-        indref,rowref,stref,wdref,vref = scanKernel(wlist,slist,x)
-        kern = np.zeros(x.shape,dtype=float)
-        kern = fillKernel(width=wdref,strength=stref,kern=kern)
-        fig,axs = plt.subplots(1,4)
-        proj = np.roll(np.roll(kern,-indref,axis=0),rowref,axis=1)
-        axs[0].imshow(proj,origin='lower') # finally, this seems correct (based on single spike images)
-        axs[0].set_title('st%.1f, wd%.1f, v%.1f'%(stref,wdref,vref))
-        axs[1].imshow(x,origin='lower')
-        axs[1].set_title('Ximg')
-        axs[2].imshow(y,origin='lower')
-        axs[2].set_title('Ypdf')
-        axs[3].imshow(x-np.inner(x.flatten(),proj.flatten())*proj,origin='lower')
-        axs[3].set_title('removed')
-        plt.show()
+        for k in shotkeys[:2]:
+            x = np.copy(f[k]['Ximg'][()]).astype(int) # deep copy to preserve original
+            y = np.copy(f[k]['Ypdf'][()]).astype(float) # deep copy to preserve original
+            wlist = f[k].attrs['sasewidth']*np.arange(.2,1.1,.1,dtype=float)
+            print(wlist)
+            slist = f[k].attrs['kickstrength']*np.arange(.25,4,.125,dtype=float)
+            print(slist)
+            temat = np.zeros(x.shape,dtype=float)
+    
+            indref,rowref,stref,wdref,vref = scanKernel(wlist,slist,x)
+            kern = np.zeros(x.shape,dtype=float)
+            kern = fillKernel(width=wdref,strength=stref,kern=kern)
+    
+            proj = np.roll(np.roll(kern,-indref,axis=0),rowref,axis=1)
+            coeff = np.inner(x.flatten(),proj.flatten())
+    
+    
+            fig,axs = plt.subplots(2,4)
+    
+            axs[0][0].imshow(proj,origin='lower') # finally, this seems correct (based on single spike images)
+            axs[0][0].set_title('st%.1f, wd%.1f, v%.1f'%(stref,wdref,vref))
+            axs[0][1].imshow(x,origin='lower')
+            axs[0][1].set_title('Ximg')
+            x -= (coeff*proj).astype(int)
+            temat[indref,rowref] += coeff
+    
+            axs[0][2].imshow(x,origin='lower')
+            axs[0][2].set_title('rm1')
+    
+            indref,rowref,stref,wdref,vref = scanKernel(wlist,slist,x)
+            kern = np.zeros(x.shape,dtype=float)
+            kern = fillKernel(width=wdref,strength=stref,kern=kern)
+    
+            proj = np.roll(np.roll(kern,-indref,axis=0),rowref,axis=1)
+            coeff = np.inner(x.flatten(),proj.flatten())
+    
+            x -= (coeff*proj).astype(int)
+            temat[indref,rowref] += coeff
+    
+            axs[0][3].imshow(x,origin='lower')
+            axs[0][3].set_title('rm2')
+            
+    
+            indref,rowref,stref,wdref,vref = scanKernel(wlist,slist,x)
+            kern = np.zeros(x.shape,dtype=float)
+            kern = fillKernel(width=wdref,strength=stref,kern=kern)
+    
+            proj = np.roll(np.roll(kern,-indref,axis=0),rowref,axis=1)
+            coeff = np.inner(x.flatten(),proj.flatten())
+    
+            x -= (coeff*proj).astype(int)
+            temat[indref,rowref] += coeff
+    
+            axs[1][0].imshow(x,origin='lower')
+            axs[1][0].set_title('rm3')
+    
+    
+            indref,rowref,stref,wdref,vref = scanKernel(wlist,slist,x)
+            kern = np.zeros(x.shape,dtype=float)
+            kern = fillKernel(width=wdref,strength=stref,kern=kern)
+    
+            proj = np.roll(np.roll(kern,-indref,axis=0),rowref,axis=1)
+            coeff = np.inner(x.flatten(),proj.flatten())
+    
+            x -= (coeff*proj).astype(int)
+            temat[indref,rowref] += coeff
+    
+            axs[1][1].imshow(x,origin='lower')
+            axs[1][1].set_title('rm4')
+    
+            axs[1][2].imshow(temat,origin='lower')
+            axs[1][2].set_title('time-energy')
+    
+            axs[1][3].imshow(y,origin='lower')
+            axs[1][3].set_title('Ypdf')
+            plt.show()
 
     return
 
