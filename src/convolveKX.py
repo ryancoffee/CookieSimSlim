cb58f49c496936963d413a0e5b90187e47e27f2e
Ryan Coffee <coffeer76@gmail.com>
2024-03-18 21:56:57 -0700

    OK, scanning is ok

--- src/convolveKX.py
+++ src/convolveKX.py
@@ -16,18 +16,39 @@ def kernConv1d(kmat,xmat):
             res = tmp
             ind = i
             rmax = np.argmax(res)
-    return ind,rmax,res
+            vmax = res[rmax]
+    return ind,rmax,vmax,res
 
 def conv1d(xmat,ymat):
-    #return np.fft.ifft(np.fft.fft(xmat,axis=1)*np.fft.fft(np.flip(ymat,axis=1),axis=1),axis=1)
     return np.fft.ifft(np.fft.fft(xmat,axis=1)*np.fft.fft(np.flip(ymat,axis=1),axis=1),axis=1)
 
-def fillKernel(f,k,kern):
+def scanKernel(widths,strengths,xmat):
+    kmat = np.zeros(xmat.shape,dtype=float)
+    vref = 0.
+    stref = 0.
+    wdref = 0.
+    indref = 0
+    rowref = 0
+    for st in strengths:
+        for wd in widths:
+            kmat = fillKernel(wd,st,kmat)
+            ind,row,vmax,res = kernConv1d(kmat,xmat)
+            if vmax>vref:
+                indref = ind
+                rowref = row
+                wdref = wd
+                stref = st
+                vref = vmax
+                print(vmax)
+    return indref,rowref,stref,wdref,vref
+
+def fillKernel(width,strength,kern):
     for r in range(kern.shape[1]):
-        w = f[k].attrs['sasewidth']
-        c = float(kern.shape[0]>>1) + f[k].attrs['kickstrength'] * np.sin(r*2*np.pi/float(kern.shape[1]))
+        w = width 
+        c = float(kern.shape[0]>>1) + strength * np.sin(r*2*np.pi/float(kern.shape[1]))
         kern[r,:] = gauss(np.arange(kern.shape[0]),w,c)
-    return kern
+    integral = np.sum(kern)
+    return kern/integral
 
 def main(fname):
     rng = np.random.default_rng()
@@ -37,19 +58,27 @@ def main(fname):
         k = shotkeys[0]
         x = f[k]['Ximg'][()]
         y = f[k]['Ypdf'][()]
+        wlist = f[k].attrs['sasewidth']*np.arange(.5,2,.1,dtype=float)
+        print(wlist)
+        slist = f[k].attrs['kickstrength']*np.arange(.5,2,.1,dtype=float)
+        print(slist)
+        indref,rowref,stref,wdref,vref = scanKernel(wlist,slist,y)
         kern = np.zeros(x.shape,dtype=float)
-        kern = fillKernel(f,k,kern)
+        kern = fillKernel(width=wdref,strength=stref,kern=kern)
+        #ix,jx,xmx,resx = kernConv1d(kern,x)
+        #iy,jy,ymx,resy = kernConv1d(kern,y)
         fig,axs = plt.subplots(1,4)
-        ix,jx,resx = kernConv1d(kern,x)
-        iy,jy,resy = kernConv1d(kern,y)
-        axs[0].plot(resx,label='resx, %i,%i'%(ix,jx))
-        axs[0].plot(resy,label='resy, %i,%i'%(iy,jy))
-        axs[0].legend()
+        #axs[0].plot(resx,label='resx, %i,%i'%(ix,jx))
+        #axs[0].plot(resy,label='resy, %i,%i'%(iy,jy))
+        #axs[0].legend()
+        axs[0].imshow(np.roll(np.roll(kern,indref,axis=0),rowref,axis=1))
+        axs[0].set_title('%i,%i,%i,%i,%i'%(indref,rowref,int(stref),int(wdref),int(vref)))
         axs[1].imshow(x)
         axs[1].set_title('Ximg')
         axs[2].imshow(y)
         axs[2].set_title('Ypdf')
         axs[3].imshow(kern)
+        #axs[3].imshow(np.roll(np.roll(kern,ix,axis=0),iy,axis=1))
         axs[3].set_title('kernel')
         plt.show()
 
