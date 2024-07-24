85dd9531da440255cd30472e48925b46106647e8
Ryan Coffee <coffeer76@gmail.com>
2024-07-24 09:59:00 -0700

    reoganize

---
+++ src/archive/mrs_pacman_old.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import numpy as np
+import h5py
+import sys
+import matplotlib.pyplot as plt
+from utils import gauss
+
+def main(fname):
+    with h5py.File(fname,'r') as f:
+        keylist = list(f.keys())
+        for k in keylist[:3]:
+            print(f[k].attrs.keys())
+            X = f[k]['Ypdf'][()]
+            #X = f[k]['Ximg'][()]
+            plt.imshow(X)
+            plt.show()
+            XFT = np.fft.fft2(X)
+            kern = np.zeros(XFT.shape,float)
+            for r in range(kern.shape[1]):
+                w = f[k].attrs['sasewidth']
+                c = float(kern.shape[0]>>1) + f[k].attrs['kickstrength'] * np.sin(r*2*np.pi/float(kern.shape[1]))
+                kern[r,:] = gauss(np.arange(kern.shape[0]),w,c)
+            norm = np.sqrt(np.inner(kern.flatten(),kern.flatten()))
+            kernFT = np.fft.fft2(kern)
+            res = np.fft.ifft2(XFT*kernFT).real
+            inds = np.where(res==np.max(res))
+            print(inds)
+            plt.imshow(res)
+            plt.title('%i,%i'%(inds[0],inds[1]))
+            plt.colorbar()
+            plt.show()
+            remove = np.roll(np.roll(kern,inds[0]-kern.shape[0],axis=0),inds[1]-kern.shape[1],axis=1)
+            #remove /=norm
+            #plt.imshow(X+1e2*remove)
+            nextim = X-.15*np.inner(X,remove)*remove
+            nextim *= (nextim>0)
+            plt.imshow(nextim)
+            plt.show()
+
+    return
+
+if __name__ == '__main__':
+    if len(sys.argv)<2:
+        print('syntax: ./src/run_pacman.py path-to-h5filename.h5')
+    else:
+        main(sys.argv[1])
--- src/mrs_pacman.py
+++ src/mrs_pacman.py
@@ -1,47 +1,154 @@
 #!/usr/bin/python3
 
-import numpy as np
-import h5py
 import sys
+import h5py
+import numpy as np
 import matplotlib.pyplot as plt
-from utils import gauss
+import math
+import time
 
-def main(fname):
+from utils import gauss,addGauss2d,addGauss2d_padding_10
+
+NSHOTS = 1<<6
+TEPROD = 1.62
+TWIN=4./.3 #4 micron streaking/(.3microns/fs)
+EWIN=100. #100 eV window
+
+def kernConv1d(kmat,xmat):
+    res = np.zeros(xmat.shape[1],dtype=float)
+    ind = 0
+    rmax = 0
+    for i in range(xmat.shape[0]):
+        tmp = np.sum( np.fft.ifft(np.fft.fft(np.roll(xmat,i,axis=0),axis=1)*np.fft.fft(np.flip(kmat,axis=1),axis=1),axis=1).real, axis=0)
+        if np.max(tmp)>np.max(res):
+            res = tmp
+            ind = i
+            rmax = np.argmax(res)
+            vmax = res[rmax]
+    return ind,rmax,vmax,res
+
+def conv1d(xmat,ymat):
+    return np.fft.ifft(np.fft.fft(xmat,axis=1)*np.fft.fft(np.flip(ymat,axis=1),axis=1),axis=1)
+
+def scanKernel(widths,strengths,xmat):
+    kmat = np.zeros(xmat.shape,dtype=float)
+    vref = 0.
+    stref = 0.
+    wdref = 0.
+    indref = 0
+    rowref = 0
+    if not np.max(xmat)>0:
+        return indref,rowref,stref,wdref,vref
+    for st in strengths:
+        for wd in widths:
+            kmat = fillKernel(wd,st,kmat)
+            ind,row,vmax,res = kernConv1d(kmat,xmat)
+            if vmax>vref:
+                indref = ind
+                rowref = row
+                wdref = wd
+                stref = st
+                vref = vmax
+    return indref,rowref,stref,wdref,vref
+
+def fillKernel(width,strength,kern):
+    for r in range(kern.shape[1]):
+        w = width 
+        c = float(kern.shape[0]>>1) + strength * np.sin(r*2*np.pi/float(kern.shape[1]))
+        kern[r,:] = gauss(np.arange(kern.shape[0]),w,c)
+    #norm = math.sqrt(np.sum(kern))
+    norm = math.sqrt(np.inner(kern.flatten(),kern.flatten()))
+    return kern/norm
+
+def main(fname,plotting=False):
+    rng = np.random.default_rng()
+    print('For now assuming 4 micron wavelength streading for a 13 fs temporal window')
+    print('For now also assuming 100eV for the energy window regardless of sampling')
+    print('.1eV = 100fs, 1eV = 10fs, (30nm width @ 800nm) 50meV = 33fs gives time-bandwidth product dE[eV]*dt[fs] = %f'%TEPROD)
+    runtimes = []
     with h5py.File(fname,'r') as f:
-        keylist = list(f.keys())
-        for k in keylist[:3]:
-            print(f[k].attrs.keys())
-            X = f[k]['Ypdf'][()]
-            #X = f[k]['Ximg'][()]
-            plt.imshow(X)
-            plt.show()
-            XFT = np.fft.fft2(X)
-            kern = np.zeros(XFT.shape,float)
-            for r in range(kern.shape[1]):
-                w = f[k].attrs['sasewidth']
-                c = float(kern.shape[0]>>1) + f[k].attrs['kickstrength'] * np.sin(r*2*np.pi/float(kern.shape[1]))
-                kern[r,:] = gauss(np.arange(kern.shape[0]),w,c)
-            norm = np.sqrt(np.inner(kern.flatten(),kern.flatten()))
-            kernFT = np.fft.fft2(kern)
-            res = np.fft.ifft2(XFT*kernFT).real
-            inds = np.where(res==np.max(res))
-            print(inds)
-            plt.imshow(res)
-            plt.title('%i,%i'%(inds[0],inds[1]))
-            plt.colorbar()
-            plt.show()
-            remove = np.roll(np.roll(kern,inds[0]-kern.shape[0],axis=0),inds[1]-kern.shape[1],axis=1)
-            #remove /=norm
-            #plt.imshow(X+1e2*remove)
-            nextim = X-.15*np.inner(X,remove)*remove
-            nextim *= (nextim>0)
-            plt.imshow(nextim)
-            plt.show()
+        shotkeys = [k for k in f.keys() if len(f[k].attrs['sasecenters'])>1]
+        rng.shuffle(shotkeys)
+        for k in shotkeys[:NSHOTS]:
+            t0 = time.time()
+            x = np.copy(f[k]['Ximg'][()]).astype(int) # deep copy to preserve original
+            y = np.copy(f[k]['Ypdf'][()]).astype(float) # deep copy to preserve original
+            tstep = TWIN/x.shape[0]
+            estep = EWIN/x.shape[1]
+            wlist = f[k].attrs['sasewidth']*estep*np.arange(.25,1.75,.125,dtype=float)
+            print(wlist)
+            slist = f[k].attrs['kickstrength']*np.arange(.25,2,.125,dtype=float)
+            print(slist)
+            temat = np.zeros(x.shape,dtype=float)
+            tedist = np.zeros((x.shape[0]+10,x.shape[1]+10),dtype=float)
+            kern = np.zeros(x.shape,dtype=float)
 
+            estep=EWIN/float(tedist.shape[1])
+            tstep=TWIN/float(tedist.shape[0])
+    
+            clow=0
+            chigh=20
+            if plotting:
+                fig,axs = plt.subplots(3,4)
+    
+                #axs[0][0].imshow(proj,origin='lower') 
+                #axs[0][0].set_title('st%.1f, wd%.1f, v%.1f'%(stref,wdref,vref))
+                axs[0][0].imshow(x,origin='lower',vmin=clow,vmax=chigh)
+                axs[0][0].set_title('Ximg')
+
+
+            for i in range(4):
+                # HERE HERE HERE  Need to add a threshold on coeff in order to count subspikes, and make a confusion matrix
+                indref,rowref,stref,ewidth,vref = scanKernel(wlist,slist,x)
+                kern = fillKernel(width=ewidth,strength=stref,kern=kern)
+                twidth=float(TEPROD)/ewidth
+    
+                proj = np.roll(np.roll(kern,-indref,axis=0),rowref,axis=1)
+                coeff = np.inner(x.flatten(),proj.flatten())
+                print(coeff/vref)
+                x -= (coeff*proj).astype(int)
+                temat[(indref+(temat.shape[1]>>1))%temat.shape[1],
+                        (rowref+(temat.shape[0]>>1))%temat.shape[0]] += coeff
+                addGauss2d_padding_10(tedist,coeff,(rowref+(tedist.shape[0]>>1))%tedist.shape[0],(indref+(tedist.shape[1]>>1))%tedist.shape[1],ewidth/estep,twidth/tstep)
+    
+                if plotting:
+                    axs[(i+1)//4][(i+1)%4].imshow(x,vmin=clow,vmax=chigh,origin='lower')
+                    axs[(i+1)//4][(i+1)%4].set_title('rm_%i'%i)
+    
+            if plotting:
+                axs[-1][-3].imshow(tedist,origin='lower')
+                axs[-1][-3].set_title('tedist')
+                axs[-1][-2].imshow(temat,origin='lower')
+                #axs[-1][-2].imshow(np.roll(np.roll(temat,temat.shape[0]//2,axis=0),temat.shape[1]//2,axis=1),origin='lower')
+                axs[-1][-2].set_title('temat')
+                axs[-1][-1].imshow(y,origin='lower')
+                axs[-1][-1].set_title('Ypdf')
+                plt.show()
+
+            t1=time.time()
+            runtimes += [t1-t0]
+            nbins = 1<<6
+    #bins = np.arange(nbins+1,dtype=float)/(nbins)*10.*1e9
+    h,b=np.histogram(runtimes,bins=1<<6)
+    _=[print('%f s:'%(b[i]) + ' '*v+'+') for i,v in enumerate(h)]
     return
 
 if __name__ == '__main__':
     if len(sys.argv)<2:
-        print('syntax: ./src/run_pacman.py path-to-h5filename.h5')
+        print('give me a file to process')
+        tedist = np.zeros((1<<7,1<<7),dtype=float)
+        ewidth=2.5
+        twidth=float(TEPROD)/ewidth
+        estep=EWIN/float(tedist.shape[1])
+        tstep=TWIN/float(tedist.shape[0])
+        print(tstep,estep,twidth,ewidth,TEPROD/ewidth)
+        print(tedist.shape[0]//3,tedist.shape[1]//3)
+        addGauss2d(tedist,1,tedist.shape[1]//2,tedist.shape[0]//2,ewidth/estep,twidth/tstep)
+        #addGauss2d(tedist,1,tedist.shape[1]//4,tedist.shape[0]//4,10,2)
+        print(np.max(tedist))
+        plt.imshow(tedist,origin='lower')
+        plt.colorbar()
+        plt.show()
+
     else:
-        main(sys.argv[1])
+        main(sys.argv[1],plotting=False)
