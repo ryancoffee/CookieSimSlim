0837d67b111fd437cb1cbcbfa0cb9ace68c1cbf0
Ryan Coffee <coffeer76@gmail.com>
2024-08-22 22:04:00 -0700

    needs to look closer

--- src/mrs_pacman.py
+++ src/mrs_pacman.py
@@ -62,13 +62,8 @@ def scanKernel(widths,strengths,xmat):
 def fillKernel(width,strength,kern):
     for r in range(kern.shape[0]):
         w = width 
-<<<<<<< HEAD
         c = float(kern.shape[1]>>1) + strength * np.sin(r*2*np.pi/float(kern.shape[0]))
         kern[r,:] = gauss(np.arange(kern.shape[1]),w,c)
-=======
-        c = float(kern.shape[0]>>1) + strength * np.sin(r*2*np.pi/float(kern.shape[1]))
-        kern[:,r] = gauss(np.arange(kern.shape[0]),w,c)
->>>>>>> 427756c (updated for laptop)
     #norm = math.sqrt(np.sum(kern))
     norm = math.sqrt(np.inner(kern.flatten(),kern.flatten()))
     return kern/norm
@@ -83,11 +78,7 @@ def main(fname,plotting=False):
         shotkeys = [k for k in f.keys() if len(f[k].attrs['sasecenters'])>1]
         rng.shuffle(shotkeys)
         oname = fname + '.tempout.h5'
-<<<<<<< HEAD
         m = re.search('^(.*/)(\w+.+\.\d+)\.h5',fname)
-=======
-        m = re.search('^(.*)(css.+)h5$',fname)
->>>>>>> 427756c (updated for laptop)
         if m:
             oname = m.group(2) + 'confusion.h5'
             opath = m.group(1) + 'output/'
@@ -191,14 +182,10 @@ def main(fname,plotting=False):
                     axs[0][0].set_title('Ximg')
 
                 cmax = 1.0
-<<<<<<< HEAD
                 cthis = 1.0
                 ewidthmean = 1.0
                 twidthmean = 1.0
                 for i in range(5):
-=======
-                for i in range(4):
->>>>>>> 427756c (updated for laptop)
                     indref,rowref,stref,ewidth,vref = scanKernel(wlist,slist,x)
                     if i==0:
                         cmax = vref
@@ -214,15 +201,11 @@ def main(fname,plotting=False):
                     #proj = np.roll(np.roll(kern,-indref,axis=0),rowref,axis=1)
                     coeff = np.inner(x.flatten(),proj.flatten())
                     coefflist += [coeff]
-<<<<<<< HEAD
+
                     cthis = coeff
                     print(coeff)
                     if cthis > THRESH:
                         rat = cthis/cmax
-=======
-                    if coeff > THRESH:
-                        rat = coeff/cmax
->>>>>>> 427756c (updated for laptop)
                     
                         if rat > 0.8:
                             nsase['80pct'] += 1;
@@ -242,7 +225,6 @@ def main(fname,plotting=False):
                     x -= (coeff*proj).astype(int)
                     proj_display += coeff*proj
 
-<<<<<<< HEAD
                     if plotting:
                         #axs[(i+1)//4][(i+1)%4].imshow(x,vmin=clow,vmax=chigh,origin='lower')
                         axs[(i+1)//4][(i+1)%4].pcolor(x)#,vmin=clow,vmax=chigh)
@@ -255,22 +237,14 @@ def main(fname,plotting=False):
                         #addGauss2d_padding_10(tedist,coeff,(rowref+(tedist.shape[0]>>1))%tedist.shape[0],(indref+(tedist.shape[1]>>1))%tedist.shape[1],ewidth/estep,twidth/tstep)
                         #temat[(indref+(temat.shape[0]>>1))%temat.shape[0],
                         #        (rowref+(temat.shape[1]>>1))%temat.shape[1]] += coeff
-                        temat[(indref),
-                                (rowref)] += coeff
+                        temat[(rowref + (temat.shape[0]>>1))%temat.shape[0],
+                                (indref + (temat.shape[1]>>1))%temat.shape[1]] += coeff
                         tedist[10:-10,10:-10] = temat
-                        tedist = cv2.GaussianBlur(tedist,(15,3),0)#,ewidthmean,twidthmean)
+                        tedist = cv2.GaussianBlur(tedist,(11,3),0)#,ewidthmean,twidthmean)
                         #addGauss2d_padding_10(tedist,coeff,indref,rowref,ewidth/estep,twidth/tstep)
                         #addGauss2d_padding_10(tedist,coeff,(rowref+(tedist.shape[1]>>1))%tedist.shape[1],(indref+(tedist.shape[0]>>1))%tedist.shape[0],ewidth/estep,twidth/tstep)
-=======
-                    if DISTRIBUTIONS and plotting:
-
-                        temat[(indref+(temat.shape[0]>>1))%temat.shape[0],
-                                (rowref+(temat.shape[1]>>1))%temat.shape[1]] += coeff
-                        addGauss2d_padding_10(tedist,coeff,(rowref+(tedist.shape[0]>>1))%tedist.shape[0],(indref+(tedist.shape[1]>>1))%tedist.shape[1],ewidth/estep,twidth/tstep)
->>>>>>> 427756c (updated for laptop)
     
     
-<<<<<<< HEAD
                     if plotting:
                         axs[-1][-4].pcolor(proj_display)
                         axs[-1][-4].set_title('sum projections')
@@ -286,16 +260,6 @@ def main(fname,plotting=False):
                         axs[-1][-1].set_title('Ypdf')
                 if plotting:
                     plt.show()
-=======
-                        if plotting:
-                            axs[-1][-3].imshow(tedist,origin='lower')
-                            axs[-1][-3].set_title('tedist')
-                            axs[-1][-2].imshow(temat,origin='lower')
-                            #axs[-1][-2].imshow(np.roll(np.roll(temat,temat.shape[0]//2,axis=0),temat.shape[1]//2,axis=1),origin='lower')
-                            axs[-1][-2].set_title('temat')
-                            axs[-1][-1].imshow(y,origin='lower')
-                            axs[-1][-1].set_title('Ypdf')
->>>>>>> 427756c (updated for laptop)
 
 
                 if plotting:
