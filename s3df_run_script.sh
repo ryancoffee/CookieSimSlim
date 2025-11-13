2f8fefb77bbce684ab6770449caaedb3c47abe16
jhirschm <jhirschm@stanford.edu>
2025-11-12 18:08:27 -0800

    organization

--- s3df_run_script.sh
+++ s3df_run_script.sh
@@ -16,7 +16,6 @@ echo starting run 1 at: `date`
 
 opath=$CSM_DATA_OUTPUT
 mkdir -p ${opath}
-# outfile=${opath}/large_even_fiveClass_Dec06_2023.h5
 outfile=${opath}/output.h5
 python3 ./src/run_simulation.py -ofname ${outfile} -n_threads 64 -n_images 8192 -n_angles 16 -n_energies 512 -polstrength 1 -polstrengthvar 1 -centralenergy 256 -centralenergyvar 256 -kickstrength 128 -kickstrengthvar 32 -drawscale 4 -custom_evenly_distributed_sase True -nclasses 5 -max_num_sase 10
 # python3 ./src/run_simulation.py -ofname ${outfile} -n_threads 64 -n_images 8192 -n_angles 16 -n_energies 512 -polstrength 1 -polstrengthvar 1 -centralenergy 256 -centralenergyvar 32 -kickstrength 128 -kickstrengthvar 32 -drawscale 4 -custom_evenly_distributed_sase True -nclasses 5 -max_num_sase 10
