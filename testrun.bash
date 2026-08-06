8bfe7f327350313896fce3f41797fc9c5fc0f13e
Ryan Coffee <coffeer76@gmail.com>
2026-08-06 07:19:34 -0700

    testrun.bash

--- testrun.bash
+++ testrun.bash
@@ -2,7 +2,8 @@
 
 nimages=128
 path_to_output='/nvme0/testrun/CookieSimSlim_data'
-offsetthreads=0
+path_to_output='/media/coffee/9C33-6BBD/CookieSimSlim_data'
+offsetthreads=64
 nthreads=4
 ./src/run_simulation.py -ofname ${path_to_output}/css.16x128.h5 -n_threads $nthreads -n_angles 16 -n_energies 128 -n_images $nimages -centralenergy 64 -centralenergyvar 32 -kickstrength 32 -polstrength 1 -polstrengthvar 0 -offset_threads $offsetthreads 
 fnum=`printf '%0*d' 3 $offsetthreads` 
